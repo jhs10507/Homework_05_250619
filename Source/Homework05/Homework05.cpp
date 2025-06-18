5e337ff3b86d3f3c6830a13206fb983f72242e11
hyunkikomori <jhs880514@naver.com>
2025-06-18 16:15:00 +0900

    업로드 테스트

---
+++ Source/Homework05/Homework05.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Homework05.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Homework05, "Homework05" );
